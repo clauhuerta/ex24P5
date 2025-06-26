fe23e4fd7878d7b1fb549f187f6a457cb776ffc6
clauhuerta <claudiahr02@icloud.com>
2025-06-26 09:49:48 +0200

    añadido caroetas back y front

---
+++ CH/src/main/java/es/ufv/dis/final2024/CH/ChApplication.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.final2024.CH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChApplication.class, args);
+	}
+
+}
---
+++ CH/src/test/java/es/ufv/dis/final2024/CH/ChApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.final2024.CH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
